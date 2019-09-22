537d77a6222aebaf4dad28c25884c0a1c8b90cd4
Tishauna Wilson <tishauna2.wilson@famu.edu>
2019-09-22 05:36:14 +0000

    Add main function to Functions.java

--- Functions.java
+++ Functions.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.Arrays;
 import java.util.Queue;
@@ -249,5 +250,21 @@ public class Functions{
 		    System.out.println("Expected : ]");
 			return false;
 		}
+	}
+	
+	public static void main (String[] args){
+		Scanner input = new Scanner(System.in);
+		String token;
+		Queue<String> storeToken = new LinkedList<>();
+		
+		token = input.next();
+		while(token.compareTo(".") != 0) {
+			if(!token.equals(" ")){
+				storeToken.add(token);
+				token = input.next();
+			}
+		}
+		Functions function = new Functions(storeToken);
+		function.validateFunctionDefinition();
 	}
 }
